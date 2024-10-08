de416507895822c261623261dec994b5c7e51c04
Jhonatan Latorre Sierra <61359329+Jhonatanls@users.noreply.github.com>
2024-10-08 13:29:23 -0500

    Solución de problemas de frontend

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,31 +1,120 @@
-let currentInput = 0;
+let valor1 = 0,
+    valor2 = 0,
+    resultado,
+    operador = "";
+
 const divPantalla = document.querySelector('#numeros-pantalla'),
-      operations = ['+', '-', '*', '/'];
+      botonesOperaciones = document.querySelectorAll('.boton-op');
 
-const inputValue = (value) => {
-    if(Number.isInteger(value) && currentInput === 0){
-        currentInput += value;
-        divPantalla.innerText = currentInput;
+divPantalla.innerText = 0;
 
-    } else if (operations.includes(value)){
-        currentInput += value;
-        animarBoton(event.target);
+const inputValue = (numero) => {
+    let aux = parseFloat(divPantalla.innerText);
+    if (aux === 0){
+        divPantalla.innerText = numero;
+    } else {
+        divPantalla.innerText += numero
     }
 }
 
 const limpiarPantalla = () => {
-    currentInput = 0;
-    divPantalla.innerText = '0';
+    divPantalla.innerText = 0;
+    valor1 = 0;
+    operador = "";
+}
+
+const operaciones = (simbolo) => {
+    operador = simbolo;
+    if(valor1 === 0){
+        valor1 = parseFloat(divPantalla.value);
+        divPantalla.innerText = 0;
+    } else {
+        switch (operador) {
+			case '+':
+				valor2 = divPantalla.value;
+				valor1 = parseFloat(valor1) + parseFloat(valor2);
+				break;
+			case '-':
+				valor2 = divPantalla.value;
+				valor1 = parseFloat(valor1) - parseFloat(valor2);
+				break;
+			case '/':
+				valor2 = divPantalla.value;
+				valor1 = parseFloat(valor1) / parseFloat(valor2);
+				break;
+			case '*':
+				valor2 = divPantalla.value;
+				valor1 = parseFloat(valor1) * parseFloat(valor2);
+				break;
+			default:
+				resultado = valor1;
+			break;
+			}
+            divPantalla.innerText = valor1;
+			divPantalla.value = 0;
+			valor2 = 0;
+    }
 }
 
 const calcular = () => {
-    try {
-        currentInput = eval(currentInput);
-        divPantalla.innerText = currentInput;
-    } catch (error) {
-        divPantalla.innerText = "Error";
+    switch (operador) {
+    case '+':
+        resultado = valor1 + parseFloat(divPantalla.value);
+        divPantalla.innerText = resultado;
+        break;
+    case '-':
+        resultado = valor1 - parseFloat(divPantalla.value);
+        divPantalla.innerText= resultado;
+        break;
+    case '/':
+        resultado = valor1 / parseFloat(divPantalla.value);
+        divPantalla.innerText = resultado;
+        break;
+    case '*':
+        resultado = valor1 * parseFloat(divPantalla.value);
+        divPantalla.innerText = resultado;
+        break;
+    default:
+        resultado = valor1;
+        divPantalla.innerText = resultado;
+    break;
+    }
+    valor1 = "";
+    operador = "";
+}
+
+const punto = () => {
+    divPantalla.innerText += ".";
+}
+
+const masMenos = () => {
+    if (divPantalla.value == "") {
+        divPantalla.innerText= "-";
+    } else {
+        divPantalla.innerText = "";
     }
 }
+
+const porcentaje = () => {
+    valor2 = parseFloat(divPantalla.value);
+    if (operador == "*") {
+        resultado = (valor1 / 100) * valor2;
+    }
+    divPantalla.innerText = resultado;
+}
+
+const removerClase = () => {
+    botonesOperaciones.forEach( button => {
+        button.classList.remove('active');
+    });
+}
+
+botonesOperaciones.forEach( button => {
+    button.addEventListener('click', () => {
+        removerClase();
+        button.classList.add('active');
+    });
+});
 
 const animarBoton = (button) => {
     button.classList.add('operation');
